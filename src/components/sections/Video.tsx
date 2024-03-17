4060a64b292c926f50f3ea25245c39cf0adb15b8
junyub301 <junyub0301@gmail.com>
2024-03-17 17:59:02 +0900

    Feat: 인트로 비디오 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -3,5 +3,16 @@ import classNames from 'classnames/bind'
 import Section from '@shared/Section'
 const cx = classNames.bind(styles)
 export default function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
