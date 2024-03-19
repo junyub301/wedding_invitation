e94a47f88ea1eba09b3e65a935e739acd9c67078
junyub301 <junyub0301@gmail.com>
2024-03-19 22:34:16 +0900

    Feat: 공유하기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Invitation from './components/sections/Invitation'
 import Calendar from './components/sections/Calendar'
 import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
+import Share from './components/sections/Share'
 
 const cx = classNames.bind(styles)
 
@@ -71,6 +72,7 @@ function App() {
       <Calendar date={date} />
       <Map location={location} />
       <Contact groom={groom} bride={bride} />
+      <Share groomName={groom.name} brideName={bride.name} date={date} />
     </div>
   )
 }
